a66d28d2ba37bd0c7bc768b820943af7ff29bb07
Petr Plenkov <petr.plenkov@booking.com>
2024-09-09 15:45:11 +0000

    update test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,12 @@
-import { test } from "@jest/globals";
+import { it, test } from "@jest/globals";
 import cds from "@sap/cds";
+import { SqliteError } from "better-sqlite3";
+
+const connected = cds.connect.to('db', {model:"."});
 
 test("CDS typer issue", async () => {
 
-  const env = cds.env;
-  //await cds.load();
-  await cds.connect.to('db', {model:"."});
+  await connected;
 
   const { OBJECTS } = await import("../@cds-models/index");
 
@@ -16,4 +17,37 @@ test("CDS typer issue", async () => {
       t?.columns?.("*");
     });
   });
+});
+
+test("CDS typer issue workaround", async () => {
+ 
+  await connected;
+
+  const { OBJECTS } = await import("../@cds-models/index");
+
+  try {
+
+    await SELECT.from(OBJECTS, (o) => {
+      o.OBJECT_TYPE, o.OBJECT_OID, o.SCHEMA_NAME, o.OBJECT_NAME;
+      o.view && o.view((t) => {
+        t("*");
+        t.columns && t.columns("*");
+      });
+    });
+    
+  } catch (error) {
+
+    if (error instanceof SqliteError) {
+
+      console.log("SqliteError occured, but the actuial call has been done! Code works");
+
+    } else {
+
+      throw error;
+
+    }
+    
+  }
+
+  
 });
